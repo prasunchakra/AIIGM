c49e31593c1bffa4e6de0a39e8a64e3e24a9c68d
prasunchakra <lordprasun@gmail.com>
2025-01-26 09:07:21 +0530

    Pivot Pivot Pivot

--- encipherx/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EncipherxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'encipherx'
--- encipherx/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'encipherx'
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('result/', views.result, name='result'),
-]
